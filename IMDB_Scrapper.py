0ca4ff06098c7c24b0dbb80d1280e53f9bcf6160
brotaku13 <brian.caulfield13@gmail.com>
2017-12-19 11:13:19 -0800

    added functionality to web scraper

--- IMDB_Scrapper.py
+++ IMDB_Scrapper.py
@@ -1,14 +1,29 @@
 from bs4 import BeautifulSoup
-import urllib.request
-import pandas as pd
 import requests
 
 r = requests.get("http://www.imdb.com/title/tt0092099/")
 soup = BeautifulSoup(r.content, "lxml")
 
+movies = {}
 for links in soup.find_all("div", class_="rec_overview"):
+    #find link
     link = links.find('a')
     print(link.get("href"))
-    rating = links.find("div", class_="rating rating-list")
-    print("title")
-    print(rating.title)
+
+    #ttcode
+    tt_code = link.get('href').split('/')[2]
+
+    #find title
+    title = link.next_element['alt']
+    print(title)
+
+    #find rating
+    div = links.find('div', class_='rating rating-list')
+    #rating = float(div.attrs['id'].split('|')[2])  this gets star rating
+    #find votes
+
+    print(title)
+
+
+
+
