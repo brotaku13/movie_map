a9450e20fdf40f29bbd42a6399974fe990b79225
brotaku13 <brian.caulfield13@gmail.com>
2017-12-19 13:25:29 -0800

    finalized topthree sorting in web scraper

--- IMDB_Scrapper.py
+++ IMDB_Scrapper.py
@@ -1,28 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
+from collections import OrderedDict
 
 r = requests.get("http://www.imdb.com/title/tt0092099/")
 soup = BeautifulSoup(r.content, "lxml")
 
 movies = {}
 for links in soup.find_all("div", class_="rec_overview"):
-    #find link
-    link = links.find('a')
-    print(link.get("href"))
+    # find link
+    link = links.find('a')  #relative hyperlink
 
-    #ttcode
+    # ttcode
     tt_code = link.get('href').split('/')[2]
 
-    #find title
+    # find title
     title = link.next_element['alt']
-    print(title)
 
-    #find rating
+    # find rating
     div = links.find('div', class_='rating rating-list')
-    #rating = float(div.attrs['id'].split('|')[2])  this gets star rating
-    #find votes
+    rating = float(div.attrs['id'].split('|')[2])
 
-    print(title)
+    # find votes
+    temp = div.attrs['title']
+
+    # following function finds number between parentheses, replaces the ',' with '' and then turns the number into an int
+    votes = int(temp[temp.find('(') + 1:temp.find(' ', temp.find('('))].replace(',', ''))
+
+    #adds information to a dictionary
+    movies[votes] = [title, tt_code, rating]
+
+
+sorted = sorted(movies)  # sorts movies, returns list of movie keys in sorted order
+topThree = {}
+for i in range(len(sorted) - 1, len(sorted) - 4, -1):  # iterates backwards over list and adds last three to a new dictionary
+    topThree[sorted[i]] = movies[sorted[i]]
+
+print(topThree)
+
+
 
 
 
