d2d4577f3c0ef8e43853e499f5c64ade1c430d97
Jonathan_Ishii <jtishii@yahoo.com>
2017-12-20 18:20:06 -0800

    Made code more efficient

--- IMDB_Scrapper.py
+++ IMDB_Scrapper.py
@@ -2,14 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from collections import OrderedDict
 
-r = requests.get("http://www.imdb.com/title/tt0092099/")
-soup = BeautifulSoup(r.content, "lxml")
-
-CONST_CONNECTIONS = 3
-rows_deep = 3
-count = 0
-
-def get_info(movie_link, CONST_CONNECTIONS, rows_deep, count):
+def get_info(movie_link, rows_deep, count):
     if(count == rows_deep):
         return
 
@@ -41,32 +34,40 @@ def get_info(movie_link, CONST_CONNECTIONS, rows_deep, count):
         movies[votes] = [title, tt_code, rating]
 
     sorted_movies = sorted(movies)  # sorts movies, returns list of movie keys in sorted order
+
     topThree = {}
 
-    for i in range(len(sorted_movies) - 1, len(sorted_movies) - 4, -1):  # iterates backwards over list and adds last three to a new dictionary
+    for i in range(-1, -4, -1):  # iterates backwards over list and adds last three to a new dictionary
         topThree[sorted_movies[i]] = movies[sorted_movies[i]]
 
     print(topThree)
 
     # creates BeautifulSoup objects to be passed into the recursive function
+    count += 1
+
     r1 = requests.get("http://www.imdb.com/title/" + tt_code_list[0])
     movie1 = BeautifulSoup(r1.content, "lxml")
+    get_info(movie1, rows_deep, count)
 
     r2 = requests.get("http://www.imdb.com/title/" + tt_code_list[1])
     movie2 = BeautifulSoup(r2.content, "lxml")
+    get_info(movie2, rows_deep, count)
 
     r3 = requests.get("http://www.imdb.com/title/" + tt_code_list[2])
     movie3 = BeautifulSoup(r3.content, "lxml")
-
-    count += 1
-    get_info(movie1, CONST_CONNECTIONS, rows_deep, count)
-    get_info(movie2, CONST_CONNECTIONS, rows_deep, count)
-    get_info(movie3, CONST_CONNECTIONS, rows_deep, count)
+    get_info(movie3, rows_deep, count)
 
 
 
 def main():
-    get_info(soup, CONST_CONNECTIONS, rows_deep, count)
+
+    r = requests.get("http://www.imdb.com/title/tt0092099/")
+    soup = BeautifulSoup(r.content, "lxml")
+
+    rows_deep = 3
+    count = 0
+
+    get_info(soup, rows_deep, count)
 
 if __name__ == "__main__":
     main()
